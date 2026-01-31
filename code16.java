7ae31b99ba68efce332dc608f384277858618512
harshiga <harshigadevi@gmail.com>
2026-01-31 21:45:40 +0530

    target num count

---
+++ code16.java
@@ -0,0 +1,16 @@
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        int c=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==target)
+            {
+                c++;
+            }
+            
+        }
+        
+        return c;
+    }
+}
